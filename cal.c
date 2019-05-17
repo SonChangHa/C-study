f668b7379b3aa8ee98baf1cd90beda8c04b51765
SonChangHa <49190602+SonChangHa@users.noreply.github.com>
2019-05-17 11:38:22 +0900

    c프 과제

---
+++ cal.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int pls(int fir, int sec) {
+
+	return fir + sec;
+}
+
+int min(int fir, int sec) {
+
+	return fir - sec;
+}
+
+int tim(int fir, int sec) {
+
+	return fir * sec;
+}
+
+int div(int fir, int sec) {
+
+	return fir / sec;
+}
+
+int input() {
+
+	int in, fir, sec, result;
+	printf("¿¬»ê ÀÔ·Â(1. µ¡¼À, 2. »¬¼À, 3. °ö¼À, 4. ³ª´°¼À. ±âÅ¸. Á¾·á)");
+	scanf_s("%d", &in);
+
+	printf("µÎ Á¤¼ö ÀÔ·Â");
+	scanf_s("%d %d", &fir, &sec);
+
+	switch (in) {
+		case 1:
+			result = pls(fir, sec);
+			break;
+		case 2:
+			result = min(fir, sec);
+			break;
+		case 3:
+			result = tim(fir, sec);
+			break;
+		case 4:
+			result = div(fir, sec);
+			break;
+	default:
+		break;
+	}
+
+	printf("°á°ú : %d", result);
+
+
+}
+
+
+int main() {
+
+	input();
+
+	return 0;
+
+
+}
